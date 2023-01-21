710644db34dd11494593915af962bed7b29aa4d4
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-01-21 14:31:21 -0600

    Section 2 - Create Category Model

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
