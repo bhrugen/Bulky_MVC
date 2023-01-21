5a39b7242b7a563a64bd6ba370386d655a7ec874
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-01-21 14:31:42 -0600

    Section 2 - Data Annotations

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,9 +1,13 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace BulkyWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
