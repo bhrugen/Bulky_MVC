6fca449ed950feca5e64885848f8a7109b7aaad0
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-01-23 19:26:56 -0600

    Section 2 - Add Category Controller

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
