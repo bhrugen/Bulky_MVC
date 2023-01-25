ee2724c23fe6e419ee0f87f6f34d598464fb5a28
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-01-25 09:24:48 -0600

    Section 2 - Display all Categories

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -14,7 +14,7 @@ namespace BulkyWeb.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
-            return View();
+            return View(objCategoryList);
         }
     }
 }
