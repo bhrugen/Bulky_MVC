51004d3a62bf10d9238862a850b71a2249156cce
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-01-25 11:08:43 -0600

    Section 2 - Input Tag Helpers

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
@@ -8,7 +9,9 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
