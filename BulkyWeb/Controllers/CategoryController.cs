9b0694ddbb47faea1206e02ebe95df72f45d7c4e
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-01-25 11:17:33 -0600

    Section 2 - Create Category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -21,5 +21,12 @@ namespace BulkyWeb.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
