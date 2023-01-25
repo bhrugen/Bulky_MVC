7963e702bfa157a516f4b9cc0bb415007694cee8
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-01-25 11:02:01 -0600

    Section 2 - Create Category UI

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -16,5 +16,10 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
