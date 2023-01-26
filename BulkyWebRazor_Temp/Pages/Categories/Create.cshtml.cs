003ad69119b478d1442d95fdb49fa55951c1ea7d
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-01-26 13:46:29 -0600

    Section 3 - Create Category Post Handler

--- BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -5,9 +5,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BulkyWebRazor_Temp.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _db;
+        
         public Category Category{ get; set; }
         public CreateModel(ApplicationDbContext db)
         {
@@ -15,6 +17,13 @@ namespace BulkyWebRazor_Temp.Pages.Categories
         }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        { 
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
         }
     }
 }
