c8f77ef6bce94ee9d6a783322bf2ccb3996f06f5
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-01-26 12:40:18 -0600

    Section 3 - Create First Razor Page

---
+++ BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
