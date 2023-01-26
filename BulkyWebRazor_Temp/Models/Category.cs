01e29546548dec78fc3ef4917c4dc797100a2f78
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-01-26 12:18:10 -0600

    Section 3 - Whats different in Razor Project?

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
