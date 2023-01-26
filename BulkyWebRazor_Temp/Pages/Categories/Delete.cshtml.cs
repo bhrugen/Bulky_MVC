e132538ab236b4a5aa133d56813c7e1f38b6e367
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-01-26 13:58:16 -0600

    Section 3 - Edit and Delete Category

---
+++ BulkyWebRazor_Temp/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,38 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public Category Category { get; set; }
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            Category? obj = _db.Categories.Find(Category.Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}
---
+++ BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,38 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public Category Category { get; set; }
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if(id !=null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
+                _db.SaveChanges();
+                //TempData["success"] = "Category updated successfully";
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
