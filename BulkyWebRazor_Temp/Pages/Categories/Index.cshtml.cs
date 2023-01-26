3daeabc892189cd702ab308d1058dd7b24d6cde8
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-01-26 12:51:22 -0600

    Section 3 - Display all categories

--- BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,16 @@ namespace BulkyWebRazor_Temp.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public List<Category> CategoryList { get; set; }
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            CategoryList = _db.Categories.ToList();
         }
     }
 }
