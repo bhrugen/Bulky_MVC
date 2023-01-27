4ddfb6430d659c252ec0cf596e78a3d9f3d8bbb3
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-01-27 13:01:50 -0600

    Section 5 - IRepository Interface

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    internal interface IRepository<T> where T : class
+    {
+        //T - Category
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
