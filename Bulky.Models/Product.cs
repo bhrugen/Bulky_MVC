c5977b38000a94a9ed1fad87717132750342b520
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-02 15:03:49 -0600

    Section 6 - Create Product Model

---
+++ Bulky.Models/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1, 1000)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 1-50")]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+
+
+        [Required]
+        [Display(Name = "Price for 50+")]
+        [Range(1, 1000)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 100+")]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
+
+    }
+}
