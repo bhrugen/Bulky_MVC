e103032e16eef76aac6f620653b3b66180e7db8c
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-03 11:46:35 -0600

    Section 6 - Projections in EF Core

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,8 @@
 using BulkyBook.DataAcess.Data;
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
@@ -16,6 +18,12 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category
+                .GetAll().Select(u=> new SelectListItem
+            {
+                Text = u.Name,
+                Value=u.Id.ToString()
+            });
             return View(objProductList);
         }
 
