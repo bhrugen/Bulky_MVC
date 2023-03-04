df476b9effc6f65c29943d6935c00d7af2fcd52e
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-04 11:57:58 -0600

    Section 7 - Details Action Method

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             return View(productList);
         }
 
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u=>u.Id== productId, includeProperties: "Category");
+            return View(product);
+        }
+
         public IActionResult Privacy()
         {
             return View();
