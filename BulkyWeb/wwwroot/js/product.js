960fa3c134b6b19eff2582752350c6cf82f01805
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-04 09:55:16 -0600

    Section 6 - Load DataTables

---
+++ BulkyWeb/wwwroot/js/product.js
@@ -0,0 +1,19 @@
+
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url:'/admin/product/getall'},
+        "columns": [
+            { data: 'title', "width": "25%" },
+            { data: 'isbn', "width": "15%" },
+            { data: 'listPrice', "width": "10%" },
+            { data: 'author', "width": "20%" },
+            { data: 'category.name', "width": "15%" }
+        ]
+    });
+}
+
