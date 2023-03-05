1629bc197ade8db6060e2513d73bff1fc0af036a
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-04 20:46:46 -0600

    Section 8 - Add Identity Tables

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
-
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
@@ -31,7 +31,7 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
