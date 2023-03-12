0b2d04eb505ac36b19df9ea9a4df6aab2c265910
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-11 23:14:28 -0600

    Section 10 - Fix issue with Add to Cart

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -43,7 +43,20 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
             shoppingCart.ApplicationUserId= userId;
 
-            _unitOfWork.ShoppingCart.Add(shoppingCart);
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u=>u.ApplicationUserId == userId &&
+            u.ProductId==shoppingCart.ProductId);
+
+            if (cartFromDb != null) {
+                //shopping cart exists
+                cartFromDb.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+            else {
+                //add cart record
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+            }
+
+            
             _unitOfWork.Save();
 
 
