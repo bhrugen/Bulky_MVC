e4e2b901659eeb96bab9bc54e59f7e9817778c07
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-12 14:19:55 -0500

    Section 10 - Get order total in Shopping Cart

--- Bulky.Models/ShoppingCart.cs
+++ Bulky.Models/ShoppingCart.cs
@@ -22,5 +22,8 @@ namespace BulkyBook.Models {
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
-    }
+
+		[NotMapped]
+		public double Price { get; set; }
+	}
 }
--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -27,7 +28,26 @@ namespace BulkyBookWeb.Areas.Customer.Controllers {
 				includeProperties: "Product")
 			};
 
+			foreach(var cart in ShoppingCartVM.ShoppingCartList) {
+				double price = GetPriceBasedOnQuantity(cart);
+				ShoppingCartVM.OrderTotal += (price * cart.Count);
+			}
+
 			return View(ShoppingCartVM);
+		}
+
+		private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart) {
+			if (shoppingCart.Count <= 50) {
+				return shoppingCart.Product.Price;
+			}
+			else {
+				if (shoppingCart.Count <= 100) {
+					return shoppingCart.Product.Price50;
+				}
+				else {
+					return shoppingCart.Product.Price100;
+				}
+			}
 		}
 	}
 }
