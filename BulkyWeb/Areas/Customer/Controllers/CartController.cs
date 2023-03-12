214a5eff1f996a2953a0f1cb7ea1ad539c09618b
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-12 14:41:08 -0500

    Section 10 - Update Quantity in Shopping Cart

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -7,47 +7,80 @@ using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers {
 
-	[Area("customer")]
-	[Authorize]
-	public class CartController : Controller {
+    [Area("customer")]
+    [Authorize]
+    public class CartController : Controller {
 
-		private readonly IUnitOfWork _unitOfWork;
-		public ShoppingCartVM ShoppingCartVM { get; set; }
-		public CartController(IUnitOfWork unitOfWork) {
-			_unitOfWork = unitOfWork;
-		}
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork) {
+            _unitOfWork = unitOfWork;
+        }
 
 
-		public IActionResult Index() {
+        public IActionResult Index() {
 
-			var claimsIdentity = (ClaimsIdentity)User.Identity;
-			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-			ShoppingCartVM = new() {
-				ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
-				includeProperties: "Product")
-			};
+            ShoppingCartVM = new() {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product")
+            };
 
-			foreach(var cart in ShoppingCartVM.ShoppingCartList) {
-				cart.Price = GetPriceBasedOnQuantity(cart);
-				ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
-			}
+            foreach (var cart in ShoppingCartVM.ShoppingCartList) {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+            }
 
-			return View(ShoppingCartVM);
-		}
+            return View(ShoppingCartVM);
+        }
 
-		private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart) {
-			if (shoppingCart.Count <= 50) {
-				return shoppingCart.Product.Price;
-			}
-			else {
-				if (shoppingCart.Count <= 100) {
-					return shoppingCart.Product.Price50;
-				}
-				else {
-					return shoppingCart.Product.Price100;
-				}
-			}
-		}
-	}
+
+        public IActionResult Plus(int cartId) {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            cartFromDb.Count += 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId) {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            if (cartFromDb.Count <= 1) {
+                //remove that from cart
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            }
+            else {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId) {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+        private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart) {
+            if (shoppingCart.Count <= 50) {
+                return shoppingCart.Product.Price;
+            }
+            else {
+                if (shoppingCart.Count <= 100) {
+                    return shoppingCart.Product.Price50;
+                }
+                else {
+                    return shoppingCart.Product.Price100;
+                }
+            }
+        }
+    }
 }
