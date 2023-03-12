b4e9d54e13acccda673dbfd0885557ff3044977f
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-11 22:25:43 -0600

    Section 9 - Register Company User

--- BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -168,6 +168,11 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
                 user.State = Input.State;
                 user.PostalCode = Input.PostalCode;
                 user.PhoneNumber = Input.PhoneNumber;
+
+                if (Input.Role == SD.Role_Company) {
+                    user.CompanyId=Input.CompanyId;
+                }
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded) {
