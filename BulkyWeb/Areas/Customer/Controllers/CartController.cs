be9c39c0195b008f69ca9c38dd256b4172af75ad
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-12 14:20:49 -0500

    Section 10 - Get order total in Shopping Cart

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -29,8 +29,8 @@ namespace BulkyBookWeb.Areas.Customer.Controllers {
 			};
 
 			foreach(var cart in ShoppingCartVM.ShoppingCartList) {
-				double price = GetPriceBasedOnQuantity(cart);
-				ShoppingCartVM.OrderTotal += (price * cart.Count);
+				cart.Price = GetPriceBasedOnQuantity(cart);
+				ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
 			}
 
 			return View(ShoppingCartVM);
