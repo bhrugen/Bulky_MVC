f8416330c282142f711fd33cb8eb4be0b9fba94c
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-12 15:44:55 -0500

    Section 11 - Order Status

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -13,5 +13,18 @@ namespace BulkyBook.Utility
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
 
-    }
+		public const string StatusPending = "Pending";
+		public const string StatusApproved = "Approved";
+		public const string StatusInProcess = "Processing";
+		public const string StatusShipped = "Shipped";
+		public const string StatusCancelled = "Cancelled";
+		public const string StatusRefunded = "Refunded";
+
+		public const string PaymentStatusPending = "Pending";
+		public const string PaymentStatusApproved = "Approved";
+		public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+		public const string PaymentStatusRejected = "Rejected";
+
+
+	}
 }
