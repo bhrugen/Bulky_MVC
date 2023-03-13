4f90e66b3d027b80f45cc9caac6b09467f76fbce
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-13 15:44:56 -0500

    Section 14 - IDbInitializer

---
+++ Bulky.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.DbInitializer {
+    public class DbInitializer : IDbInitializer {
+        public void Initialize() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bulky.DataAccess/DbInitializer/IDbInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.DbInitializer {
+    public interface IDbInitializer {
+        void Initialize();
+    }
+}
