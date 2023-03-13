731cf35088d74399f766c3069426a4cdfa63118a
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-13 13:48:58 -0500

    Section 13 - Facebook Social Login

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -22,6 +22,10 @@ builder.Services.ConfigureApplicationCookie(options => {
     options.LogoutPath = $"/Identity/Account/Logout";
     options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
 });
+builder.Services.AddAuthentication().AddFacebook(option => {
+    option.AppId = "193813826680436";
+    option.AppSecret = "8fc42ae3f4f2a4986143461d4e2da919";
+});
 
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options => {
