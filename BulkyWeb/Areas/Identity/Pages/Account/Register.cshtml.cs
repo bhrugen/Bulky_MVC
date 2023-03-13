54e0df86807f0ad0c57762a4f83cf8f8da710327
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-13 15:38:51 -0500

    Section 13 - Creating Admin and Employee Accounts

--- BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -203,7 +203,12 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin)) {
+                            TempData["success"] = "New User Created Successfully";
+                        }
+                        else {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
