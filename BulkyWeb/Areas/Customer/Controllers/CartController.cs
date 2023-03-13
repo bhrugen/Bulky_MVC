60d9b4db385a93324abc13bef8d872bac9ab4a14
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-12 23:40:04 -0500

    Section 11 - Stripe in Action

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
 using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers {
@@ -111,8 +112,38 @@ namespace BulkyBookWeb.Areas.Customer.Controllers {
             }
 
 			if (applicationUser.CompanyId.GetValueOrDefault() == 0) {
-				//it is a regular customer account and we need to capture payment
-				//stripe logic
+                //it is a regular customer account and we need to capture payment
+                //stripe logic
+                var domain = "https://localhost:7169/";
+				var options = new SessionCreateOptions {
+					SuccessUrl = domain+ $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
+                    CancelUrl = domain+"customer/cart/index",
+					LineItems = new List<SessionLineItemOptions>(),
+					Mode = "payment",
+				};
+
+                foreach(var item in ShoppingCartVM.ShoppingCartList) {
+                    var sessionLineItem = new SessionLineItemOptions {
+                        PriceData = new SessionLineItemPriceDataOptions {
+                            UnitAmount = (long)(item.Price * 100), // $20.50 => 2050
+                            Currency = "usd",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions {
+                                Name = item.Product.Title
+                            }
+                        },
+                        Quantity = item.Count
+                    };
+                    options.LineItems.Add(sessionLineItem);
+                }
+
+
+				var service = new SessionService();
+				Session session = service.Create(options);
+                _unitOfWork.OrderHeader.UpdateStripePaymentID(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+                _unitOfWork.Save();
+                Response.Headers.Add("Location", session.Url);
+                return new StatusCodeResult(303);
+
 			}
 
 			return RedirectToAction(nameof(OrderConfirmation),new { id=ShoppingCartVM.OrderHeader.Id });
