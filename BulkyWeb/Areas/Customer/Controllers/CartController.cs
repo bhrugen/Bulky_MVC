ca6027a7d71474dc6536e4254d5b7d198d2175a8
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-13 13:05:52 -0500

    Section 13 - Bug Solution and Logout

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -187,7 +187,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers {
         }
 
         public IActionResult Minus(int cartId) {
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked: true);
             if (cartFromDb.Count <= 1) {
                 //remove that from cart
                 HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
@@ -204,7 +204,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers {
         }
 
         public IActionResult Remove(int cartId) {
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId,tracked:true);
             HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
                 .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
             _unitOfWork.ShoppingCart.Remove(cartFromDb);
--- BulkyWeb/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ BulkyWeb/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -26,6 +26,7 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
+            HttpContext.Session.Clear();
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
