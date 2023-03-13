70b868b060e4b57374695f2dfb1f5a3240b86ca5
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-13 00:36:15 -0500

    Section 12 - Order Details Get Action

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -18,13 +19,23 @@ namespace BulkyBookWeb.Areas.Admin.Controllers {
         }
 
         public IActionResult Index() {
-			return View();
-		}
+            return View();
+        }
+
+        public IActionResult Details(int orderId) {
+            OrderVM orderVM = new() {
+                OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
+            };
+
+            return View(orderVM);
+        }
 
 
-		#region API CALLS
 
-		[HttpGet]
+        #region API CALLS
+
+        [HttpGet]
 		public IActionResult GetAll(string status) {
 			IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
 
