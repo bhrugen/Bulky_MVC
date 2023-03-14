4e41cedadb61824dbba882e45024b3a1f29f9616
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-13 20:10:14 -0500

    Section 14 - SendGrid Email Setup

--- Bulky.Utility/EmailSender.cs
+++ Bulky.Utility/EmailSender.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid.Helpers.Mail;
+using SendGrid;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +10,25 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.Utility {
     public class EmailSender : IEmailSender {
+        public string SendGridSecret { get; set; }
+
+        public EmailSender(IConfiguration _config) {
+            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+        }
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage) {
             //logic to send email
-            return Task.CompletedTask;
+
+            var client = new SendGridClient(SendGridSecret);
+
+            var from = new EmailAddress("hello@dotnetmastery.com", "Bulk Book");
+            var to = new EmailAddress(email);
+            var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
+            return client.SendEmailAsync(message);
+
+
         }
     }
 }
+
