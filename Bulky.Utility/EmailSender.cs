7231e852ce843a1e2303206d46614db1895a1e2e
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-13 20:32:51 -0500

    Section 14 - SendGrid in Action

--- Bulky.Utility/EmailSender.cs
+++ Bulky.Utility/EmailSender.cs
@@ -21,7 +21,7 @@ namespace BulkyBook.Utility {
 
             var client = new SendGridClient(SendGridSecret);
 
-            var from = new EmailAddress("hello@dotnetmastery.com", "Bulk Book");
+            var from = new EmailAddress("hello@dotnetmastery.com", "Bulky Book");
             var to = new EmailAddress(email);
             var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
 
--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Stripe.Checkout;
 using System.Security.Claims;
@@ -14,10 +15,12 @@ namespace BulkyBookWeb.Areas.Customer.Controllers {
     public class CartController : Controller {
 
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IEmailSender _emailSender;
         [BindProperty]
         public ShoppingCartVM ShoppingCartVM { get; set; }
-        public CartController(IUnitOfWork unitOfWork) {
+        public CartController(IUnitOfWork unitOfWork, IEmailSender emailSender) {
             _unitOfWork = unitOfWork;
+            _emailSender = emailSender;
         }
 
 
@@ -167,6 +170,9 @@ namespace BulkyBookWeb.Areas.Customer.Controllers {
                 HttpContext.Session.Clear();
 
 			}
+
+            _emailSender.SendEmailAsync(orderHeader.ApplicationUser.Email, "New Order - Bulky Book",
+                $"<p>New Order Created - {orderHeader.Id}</p>");
 
             List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart
                 .GetAll(u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
