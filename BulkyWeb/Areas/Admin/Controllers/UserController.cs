0cb19fe6e81ddd233037daaa55d91449111d1023
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-14 19:34:45 -0500

    Section 15 - Display User List

---
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.DataAcess.Data;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index() 
+        {
+            return View();
+        }
+
+       
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u=>u.Company).ToList();
+
+            return Json(new { data = objUserList });
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
---
+++ BulkyWeb/wwwroot/js/user.js
@@ -0,0 +1,27 @@
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url:'/admin/user/getall'},
+        "columns": [
+            { "data": "name", "width": "15%" },
+            { "data": "email", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "company.name", "width": "15%" },
+            { "data": "", "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/company/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                    </div>`
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
