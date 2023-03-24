4eb877498f5f8bef7f1a1f82b18c96c09465ee3f
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-24 12:28:59 -0500

    Section 16 - Delete Image

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -121,6 +121,29 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         }
 
 
+        public IActionResult DeleteImage(int imageId) {
+            var imageToBeDeleted = _unitOfWork.ProductImage.Get(u => u.Id == imageId);
+            int productId = imageToBeDeleted.ProductId;
+            if (imageToBeDeleted != null) {
+                if (!string.IsNullOrEmpty(imageToBeDeleted.ImageUrl)) {
+                    var oldImagePath =
+                                   Path.Combine(_webHostEnvironment.WebRootPath,
+                                   imageToBeDeleted.ImageUrl.TrimStart('\\'));
+
+                    if (System.IO.File.Exists(oldImagePath)) {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+
+                _unitOfWork.ProductImage.Remove(imageToBeDeleted);
+                _unitOfWork.Save();
+
+                TempData["success"] = "Deleted successfully";
+            }
+
+            return RedirectToAction(nameof(Upsert), new { id = productId });
+        }
+
         #region API CALLS
 
         [HttpGet]
