745a09429fedf461f82cbde6c2a82f708f2efce4
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-03-26 13:00:09 -0500

    Microsoft Social Sign in

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -27,6 +27,10 @@ builder.Services.AddAuthentication().AddFacebook(option => {
     option.AppId = "193813826680436";
     option.AppSecret = "8fc42ae3f4f2a4986143461d4e2da919";
 });
+builder.Services.AddAuthentication().AddMicrosoftAccount(option => {
+    option.ClientId = "ec4d380d-d631-465d-b473-1e26ee706331";
+    option.ClientSecret = "qMW8Q~LlEEZST~SDxDgcEVx_45LJQF2cQ_rEKcSQ";
+});
 
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options => {
